a4c3323165d1488b6b183d6fec6866fbfd4dec1b
Amir <mirzayi994@gmail.com>
2023-09-30 07:35:55 +0330

    add payment url

--- internal/payment/novinpay/global/constant.go
+++ internal/payment/novinpay/global/constant.go
@@ -14,6 +14,8 @@ type PaymentURI string
 const (
 	PaymentCallback string = "https://dizintork.ir:4499/TestPayment/"
 
+	PaymentURL string = "https://pna.shaparak.ir/_ipgw_/payment/"
+
 	baseURI                       PaymentURI = "https://pna.shaparak.ir/ref-payment2/RestServices/mts"
 	LoginURI                      PaymentURI = baseURI + "/merchantLogin/"
 	GenerateTransactionDataToSign PaymentURI = baseURI + "/generateTransactionDataToSign/"
